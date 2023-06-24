e26cf09acc8946008626383a85d87632d91f2872
tvojk <tvojk13@gmail.com>
2023-06-24 20:17:58 +0300

    Commit

--- newsPortal/main/models.py
+++ newsPortal/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class News(models.Model):
+    date = models.DateTimeField('Дата')
+    header = models.CharField('Заголовок', max_length=255)
+    article = models.TextField('Статья')
+    published = models.BooleanField('Опубликовано', default=False)
+
 # Create your models here.
--- newsPortal/main/views.py
+++ newsPortal/main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import datetime
+from .models import *
 
 def index(request):
-    return render(request, 'main/index.html')
+    content = News.objects.all()
+    return render(request, 'main/index.html', {'content': content}) # .strftime('%H:%M')
